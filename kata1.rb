b270b49733f6bfa658f9bcae4d36bfc0f4480f8b
Shreyansh Gupta <shreyansh_gupta@apple.com>
2024-08-24 13:01:26 +0530

    Adds set up to split the numbers on either a comma or a newline.

--- kata1.rb
+++ kata1.rb
@@ -16,29 +16,34 @@ class Kata1Test < Minitest::Test
     end
 
     it "add returns 0 for empty string" do
-      assert_equal add(""), 0
+      assert_equal 0, add("")
     end
 
     it "add returns n if only 1 integer is provided" do
-      assert_equal add("1"), 1
-      assert_equal add("2"), 2
-      assert_equal add("3"), 3
-      assert_equal add("1234567"), 1234567
+      assert_equal 1, add("1")
+      assert_equal 2, add("2")
+      assert_equal 3, add("3")
+      assert_equal 1234567, add("1234567")
     end
 
     it "returns the sum if 2 integers are provided separated by commas" do
-      assert_equal add("1,2"), 3
-      assert_equal add("3,4"), 7
-      assert_equal add("11,21"), 32
-      assert_equal add("123,2"), 125
-      assert_equal add("1,234"), 235
+      assert_equal 3, add("1,2")
+      assert_equal 7, add("3,4")
+      assert_equal 32, add("11,21")
+      assert_equal 125, add("123,2")
+      assert_equal 235, add("1,234")
     end
 
     it "returns the sum for any amount of numbers are provided separated by commas" do
-      assert_equal add("1,2,3,4,5"), 15
-      assert_equal add("1,2,323"), 326
-      assert_equal add("112,0,32"), 144
-      assert_equal add("1,1,1,1,1,1,1,1,1,1"), 10
+      assert_equal 15, add("1,2,3,4,5")
+      assert_equal 326, add("1,2,323")
+      assert_equal 144, add("112,0,32")
+      assert_equal 10, add("1,1,1,1,1,1,1,1,1,1")
+    end
+
+    it "returns the sum if the numbers are provided separated by either a comma or a newline" do
+      assert_equal 15, add("1,2,3\n4\n5")
+      assert_equal 326, add("1\n2,323")
     end
 
     def add(input)
@@ -58,6 +63,6 @@ class Kata1
     Kata1::InvalidInput.raise_error unless numbers.is_a?(String)
     return 0 if numbers.length.zero?
 
-    numbers.split(",").sum(&:to_i)
+    numbers.split(/[,\n]{1}/).sum(&:to_i)
   end
 end
